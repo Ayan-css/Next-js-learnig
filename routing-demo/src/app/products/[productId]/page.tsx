6ac9d7cbba08ba751f5836ce15f271d47ee1d216
Ayan-css <ayan48311@gmail.com>
2025-08-15 20:26:12 +0530

    Dynamic Routing in Next.js

---
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -0,0 +1,6 @@
+import { promises } from "dns";
+
+export default async function ProductDetail({params}:{params: Promise<{ productId:string}>;}){
+    const productId = (await params).productId
+        return <h1>Details about Product {productId}</h1>
+}
